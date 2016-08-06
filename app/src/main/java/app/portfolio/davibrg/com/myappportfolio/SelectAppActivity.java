3018d1e0372e28854f3deac5a4ca03f71ac09bd3
Davi Braga <davibrg@gmail.com>
2016-08-06 10:33:09 -0300

    Added suggestions from udacity reviewer.

--- app/src/main/java/app/portfolio/davibrg/com/myappportfolio/SelectAppActivity.java
+++ app/src/main/java/app/portfolio/davibrg/com/myappportfolio/SelectAppActivity.java
@@ -6,8 +6,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-
 public class SelectAppActivity extends AppCompatActivity {
 
     @Override
@@ -15,29 +13,12 @@ public class SelectAppActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_app);
 
-        ArrayList<Button> buttonArrayList = new ArrayList<Button>();
-
-        buttonArrayList.add((Button) findViewById(R.id.button));
-        buttonArrayList.add((Button) findViewById(R.id.button2));
-        buttonArrayList.add((Button) findViewById(R.id.button3));
-        buttonArrayList.add((Button) findViewById(R.id.button4));
-        buttonArrayList.add((Button) findViewById(R.id.button5));
-        buttonArrayList.add((Button) findViewById(R.id.button6));
-
-
-        for(Button button : buttonArrayList) {
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    showAppNameToast((Button) v);
-                }
-            });
-        }
     }
 
-    private void showAppNameToast(Button button) {
-        String toastString = "This button will launch " + button.getText().toString().toLowerCase()
-                + " app!";
+    public void showAppNameToast(View v) {
+        Button button = (Button) v;
+        String toastString = String.format(getResources().getString(R.string.portfolio_toast),
+                button.getText().toString().toLowerCase());
         Toast.makeText(getApplicationContext(),toastString, Toast.LENGTH_SHORT).show();
     }
 }
